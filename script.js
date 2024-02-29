c42aeb2d692c317af897580a59aae898398c7ac9
Félix RDZ Ponte <lucardzponte@gmail.com>
2024-02-29 13:48:44 +0100

    lottie fix

--- script.js
+++ script.js
@@ -32,65 +32,31 @@ document.getElementById('lottie-animation').addEventListener('click', function()
   }
 });
 
-
-var animation2 = lottie.loadAnimation({
-    container: document.getElementById('dark-mode-animation'),
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'assets/dark-mode-animation.json'
-});
-
-var atStart2 = true;
-
-document.getElementById('dark-mode-animation').addEventListener('click', function() {
-  if (atStart2) {
-    // Reproduce el segmento de la animación desde el frame actual hasta el frame 40
-    animation2.playSegments([60, 100], true);
-    atStart2 = false;
-  } else {
-    // Reproduce el segmento de la animación desde el frame 40 de regreso al frame 0
-    animation2.playSegments([100, 40], true);
-    atStart2 = true;
-  }
-});
-
-var animation3 = lottie.loadAnimation({
-    container: document.getElementById('scroll-animation'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: false,
-    path: 'assets/scroll-animation.json'
-});
-
-document.getElementById('scroll-animation').addEventListener('click', function() {
-    animation3.play();
-});
-
 // Referencia a la animación Lottie
-// LottieInteractivity.create({
-//     player: '#dark-mode-animation',
-//     mode: 'cursor',
-//     actions: [
-//       {
-//         type: "toggle",
-//         frames: [40, 80]
-//       }
-//     ]
-// });
-// LottieInteractivity.create({
-//     player: '#scroll-animation',
-//     mode: 'scroll',
-//     actions: [
-//       {
-//         visibility: [0,1],
-//         visibility: [0.20, 1.0],
-//         type: 'seek',
-//         container: 'scroll',
-//         frames: [0, 40],
-//       }
-//     ]
-// });
+LottieInteractivity.create({
+    player: '#dark-mode-animation',
+    mode: 'cursor',
+    actions: [
+      {
+        type: "toggle",
+        frames: [40, 80]
+      }
+    ]
+});
+LottieInteractivity.create({
+    player: '#scroll-animation',
+    mode: 'scroll',
+    actions: [
+      {
+        visibility: [0,1],
+        visibility: [0.50, 1.0],
+        type: 'seek',
+        container: 'scroll',
+        frames: [0, 40],
+      }
+    ]
+});
 
+  
   
 
